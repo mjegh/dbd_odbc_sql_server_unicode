56541c60eccc7abc1905685898f92f37acf0bbee
Martin J. Evans <github@virginmedia.com>
2013-10-21 23:11:13 +0100

    move codepage stuff to a different file

--- unicode_in_varchars.pl
+++ unicode_in_varchars.pl
@@ -8,15 +8,12 @@
 # Show the diference between binding as SQL_CHAR and SQL_WCHAR.
 #
 # See http://msdn.microsoft.com/en-us/library/bb330962.aspx#intlftrql2005_topic2
-# Also contains a load of code for getting codepage info that probably
-# wants moving elsewhere eventually.
 #
 use 5.008.001;
 use strict;
 use warnings;
 use DBI qw(:sql_types);
 use Data::Dumper;
-use DBI::Const::GetInfoType;
 use Win32::API;
 
 # chcp on my machines normally gives 850
@@ -40,70 +37,9 @@ if ($cp != 1252) {
 my $h = DBI->connect() or die $DBI::errstr;
 $h->{RaiseError} = 1;
 $h->{PrintError} = 1;
-# so we can use :: not meaning placeholders
-$h->{odbc_ignore_named_placeholders} = 1;
-
-# get a list of all collations
-my $r = $h->selectall_arrayref(q/SELECT * FROM ::fn_helpcollations()/);
-# just print out the latin ones for now
-foreach my $row (@$r) {
-    print $row->[0], "\n" if $row->[0] =~ /Latin/;
-}
 
 eval {$h->do(q/drop table varchar_test/)};
 $h->do(q/create table varchar_test(a varchar(20) collate Latin1_General_CI_AS)/);
-
-# get database name to use later when finding collation for table
-my $database_name = $h->get_info($GetInfoType{SQL_DATABASE_NAME});
-print "Database: ", $database_name, "\n";
-
-# now find out the collations
-# server collation:
-$r = $h->selectrow_arrayref(
-    q/SELECT CONVERT (varchar, SERVERPROPERTY('collation'))/);
-print "Server collation: ", $r->[0], "\n";
-
-# database collation:
-$r = $h->selectrow_arrayref(
-    q/SELECT CONVERT (varchar, DATABASEPROPERTYEX(?,'collation'))/,
-   undef, $database_name);
-print "Database collation: ", $r->[0], "\n";
-
-# now call sp_help to find out about our table
-# first result-set should be name, owner, type and create datetime
-# second result-set should be:
-#  column_name, type, computed, length, prec, scale, nullable, trimtrailingblanks,
-#  fixedlennullinsource, collation
-# third result-set is identity columns
-# fourth result-set is row guilded columns
-# there are other result-sets depending on the object
-# sp_help -> http://technet.microsoft.com/en-us/library/ms187335.aspx
-my $column_collation;
-print "\nCalling sp_help for table:\n";
-my $s = $h->prepare(q/{call sp_help(?)}/);
-$s->execute("varchar_test");
-my $result_set = 1;
-do {
-    my $rows = $s->fetchall_arrayref;
-    if ($result_set <= 2) {
-        foreach my $row (@{$rows}) {
-            print join(",", map {$_ ? $_ : 'undef'} @{$row}), "\n";
-        }
-    }
-    if ($result_set == 2) {
-        foreach my $row (@{$rows}) {
-            print "column:", $row->[0], " collation:", $row->[9], "\n";
-            $column_collation = $row->[9];
-        }
-    }
-    $result_set++;
-} while $s->{odbc_more_results};
-
-# now using the last column collation from above find the codepage
-$r = $h->selectrow_arrayref(
-    q/SELECT COLLATIONPROPERTY(?, 'CodePage')/,
-    undef, $column_collation);
-print "Code page for column collation: ", $r->[0], "\n";
 
 # note codepage 1252 is 255 chrs including the euro at 0x80
 # windows-1252 does not contain U+0187 but it does contain
@@ -123,7 +59,7 @@ $insert->execute($data);
 
 print "\nNotice in the first row, the UTF-8 stored in the perl scalar is mostly stored as individual characters but then you will be wondering why the few of the characters seem to come back as unicode. Windows sees individual characters in the UTF-8 sequence as characters in the windows-1252 codepage and the UTF-8 sequence contains some characters in windows-1252 which map back to unicode chrs. e.g., the UTF-8 sequence for the euro is e2, 82, ac and windows see the 82 as the curved quotes in windows-1252 but when you ask for it back as wide/unicode characters it can map it to U+201a\n";
 print "\nNotice how in the second row the last character is a ?. That is because U+0187 does not exist in windows-1252 codepage our column is using\n";
-$r = $h->selectall_arrayref(q/select a from varchar_test/);
+my $r = $h->selectall_arrayref(q/select a from varchar_test/);
 print Dumper($r);
 foreach my $row (@$r) {
     print $row->[0], "\n";
